5a468a33dc92da57e0da634f81909cdc4525d7bd
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-05-25 11:39:23 -1000

    Initialize React TS project

---
+++ react-ts/shopping-app/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+// const num: number = 123;
+// console.log("Test from App.tsx", num);
+
+// function func<T>(x: T): T {
+//   return x;
+// }
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
