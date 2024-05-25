598467988bfae349da78401f322d8194ccc6f497
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-05-25 12:55:50 -1000

    Update State

--- react-ts/shopping-app/src/App.tsx
+++ react-ts/shopping-app/src/App.tsx
@@ -3,18 +3,21 @@ import './App.css';
 import ShoppingList from './components/ShoppingList';
 import Item from "./models/item";
 import ShoppingListForm from "./components/ShoppingListForm";
+import { v4 as getId} from "uuid";
 
 function App() {
   const [items, setItems] = useState<Item[]>([])
   const addItem = (product: string) => {
     console.log("App Component");
-    console.log(product);
-  }
+    // console.log(product);
+    // spread items
+    setItems([...items, { id: getId(), product, quantity: 1 }]);
+  };
 
   return (
     <div>
       <ShoppingList items={items} />
-      <ShoppingListForm onAddItem={addItem}/>
+      <ShoppingListForm onAddItem={addItem} />
     </div >
   );
 }
