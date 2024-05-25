9111c629285958a463848db878aa0c2d03855a5e
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-05-25 11:56:56 -1000

    Add in props

--- react-ts/shopping-app/src/App.tsx
+++ react-ts/shopping-app/src/App.tsx
@@ -1,31 +1,14 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-
-// const num: number = 123;
-// console.log("Test from App.tsx", num);
-
-// function func<T>(x: T): T {
-//   return x;
-// }
+import Greeter from './components/Greeter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greeter person="Name1" />
+      <Greeter person="Name2" />
+      <Greeter person="Name3" />
+      {/* <h1>Hello World</h1> */}
     </div>
   );
 }
---
+++ react-ts/shopping-app/src/components/Greeter.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+// function Greeter() {
+//     return <h1 >Hello!</h1>
+// }
+
+interface GreeterProps {
+    person: string;
+}
+
+function Greeter({ person }: GreeterProps): JSX.Element {
+    return <h1 >Hello, {person}!</h1>
+}
+
+// function Greeter(props: GreeterProps): JSX.Element {
+//     return <h1 >Hello, {props.person}!</h1>
+// }
+
+// function Greeter(props: { person: string }): JSX.Element {
+//     return <h1 >Hello, {props.person}!</h1>
+// }
+
+export default Greeter;
